d12eeccc8287c153853eaf406cbc84c2e304d15d
Prathyudh Prem A P <prathyudhpremap@gmail.com>
2026-01-01 12:20:22 +0530

    fix postgres config

--- expense_tracker_project/settings.py
+++ expense_tracker_project/settings.py
@@ -85,9 +85,7 @@ WSGI_APPLICATION = 'expense_tracker_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600)
 }
 
 
